7ab99ca0d2187b473b89f889fd90278ef590fadb
ruijie-robot <ruijie1024@gmail.com>
2025-07-18 23:08:44 +0800

    test chinese encoding parsing

---
+++ test/test-pdfminer.py
@@ -0,0 +1,7 @@
+from pdfminer.high_level import extract_text
+
+text = extract_text("docs/Bank_Tarrif_CN_simplified.pdf", codec="gb18030")
+# text = extract_text("docs/Bank_Tarrif_CN_Tranditional.pdf", codec="utf-8")
+# text = extract_text("docs/Bank_Tarrif_CN_simplified.pdf")
+
+print(text)
