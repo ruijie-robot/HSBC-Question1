45bd3b88d1905e4b15aeb38f6b79446c11d689c4
ruijie-robot <ruijie1024@gmail.com>
2025-07-18 23:52:36 +0800

    use Neo4j

---
+++ test/test-neo4j.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv
+import os
+import textwrap
+
+# Langchain
+from langchain_community.graphs import Neo4jGraph
+from langchain_community.vectorstores import Neo4jVector
+from langchain_openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain_openai import ChatOpenAI
+
+# Warning control
+import warnings
+warnings.filterwarnings("ignore")
+
+
+# Load from environment
+load_dotenv('.env', override=True)
+NEO4J_URI = os.getenv('NEO4J_URI')
+NEO4J_USERNAME = os.getenv('NEO4J_USERNAME')
+NEO4J_PASSWORD = os.getenv('NEO4J_PASSWORD')
+NEO4J_DATABASE = os.getenv('NEO4J_DATABASE') or 'neo4j'
+
+# Global constants
+VECTOR_INDEX_NAME = 'form_10k_chunks'
+VECTOR_NODE_LABEL = 'Chunk'
+VECTOR_SOURCE_PROPERTY = 'text'
+VECTOR_EMBEDDING_PROPERTY = 'textEmbedding'
+
+
+kg = Neo4jGraph(
+    url=NEO4J_URI, 
+    username=NEO4J_USERNAME, 
+    password=NEO4J_PASSWORD, 
+    database=NEO4J_DATABASE
+)
+
+print("stop")
